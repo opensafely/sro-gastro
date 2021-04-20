4809771e73b5b449b2b5e440ba2fa82b07dc8ff8
Nick Kennedy <nick@nick-kennedy.com>
2021-04-20 21:31:29 +0100

    Added diagnosis, referral, clinic codelists

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -16,3 +16,21 @@ qfit_codes = codelist_from_csv(
   system="snomed",
   column="code"
 )
+
+crc_diagnosis_codes = codelist_from_csv(
+  "codelists/user-NikNakk-bowel-cancer-diagnosis.csv",
+  system="snomed",
+  column="code"
+)
+
+ft_colorectal_referral_codes = codelist_from_csv(
+  "codelists/user-NikNakk-fast-track-colorectal-cancer-referral.csv",
+  system="snomed",
+  column="code"
+)
+
+ft_colorectal_clinic_codes = codelist_from_csv(
+  "codelists/user-NikNakk-seen-in-fast-track-colorectal-clinic.csv",
+  system="snomed",
+  column="code"
+)
--- analysis/model.R
+++ analysis/model.R
@@ -2,11 +2,23 @@
 # Authors: Nick Kennedy (NikNakk), Gareth Walker
 
 library(tidyverse)
+library(lubridate)
+source("sro_gastro_functions.R")
 # Read data output from cohortextractor
 input <- read_csv("output/input.csv")
 
 # Counts of qFIT by STP
 input %>%
-  filter(!is.na(qfit)) %>% 
-  count(stp) %>% 
+  group_by(stp) %>% 
+  summarise(
+    n_people = n(),
+    had_qfit = sum(!is.na(qfit_date)),
+    had_ft_colorectal_referral = sum(!is.na(ft_referral_date)),
+    had_ft_colorectal_clinic = sum(!is.na(ft_clinic_date)),
+    had_crc_diagnosis = sum(!is.na(crc_diagnosis_date)),
+    qfit_referral_timing = med_iqr((qfit_date %--% ft_referral_date) / ddays(1), na.rm = TRUE),
+    qfit_clinic_timing = med_iqr((qfit_date %--% ft_clinic_date) / ddays(1), na.rm = TRUE),
+    qfit_diagnosis_timing = med_iqr((qfit_date %--% crc_diagnosis_date) / ddays(1), na.rm = TRUE)
+  ) %>% 
   write_csv("output/qfit_by_stp.csv")
+
