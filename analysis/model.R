710eab1b624552ba439ee2600a8666b1d17aa4e7
Nick Kennedy <nick@nick-kennedy.com>
2021-04-23 19:35:52 +0100

    Rationalised output cols and added simple graphs

--- analysis/model.R
+++ analysis/model.R
@@ -21,11 +21,9 @@ input %>%
     female = sum(sex == "F"),
     had_qfit = sum(!is.na(qfit_date)),
     qfit_result = med_iqr(qfit[!is.na(qfit_date)]),
-    had_ft_colorectal_referral = sum(!is.na(ft_referral_date)),
-    had_ft_colorectal_clinic = sum(!is.na(ft_clinic_date)),
-    had_ft_colorectal_clinic2 = sum(na.omit(ft_clinic)),
-    had_crc_diagnosis = sum(!is.na(crc_diagnosis_date)),
-    had_crc_diagnosis2 = sum(na.omit(crc_diagnosis)),
+    had_ft_colorectal_referral = sum(ft_referral, na.rm = TRUE),
+    had_ft_colorectal_clinic = sum(ft_clinic, na.rm = TRUE),
+    had_crc_diagnosis = sum(crc_diagnosis, na.rm = TRUE),
     referral_if_qfit_ge10 = sum(!is.na(ft_referral_date[qfit >= 10 & !is.na(qfit_date)])),
     referral_if_qfit_lt10 = sum(!is.na(ft_referral_date[qfit < 10 & !is.na(qfit_date)])),
     referral_if_qfit_not_done = sum(!is.na(ft_referral_date[is.na(qfit_date)])),
@@ -41,5 +39,23 @@ input %>%
       paste(collapse = "; ")
   ) %>% 
   write_csv("output/qfit_by_stp.csv")
+
+qfits_by_stp <- input %>% 
+  filter(!is.na(qfit_date)) %>% 
+  count(stp, qfit = round(qfit)) %>% 
+  ggplot(aes(qfit, n)) +
+  geom_col(fill = "dark blue") +
+  facet_wrap(quos(stp), scales = "free")
+
+ggsave("output/qfits_by_stp.png", qfits_by_stp, width = 24, height = 24)
+
+qfit_months_by_stp <- input %>% 
+  filter(!is.na(qfit_date)) %>% 
+  count(stp, qfit_month = floor_date(qfit_date, "months")) %>% 
+  ggplot(aes(qfit_month, n)) +
+  geom_col(fill = "dark blue") +
+  facet_wrap(quos(stp), scales = "free")
+
+ggsave("output/qfit_months_by_stp.png", qfit_months_by_stp, width = 24, height = 24)
 
 # write_csv(as.data.frame(table(input$qfit)), "output/summary_qfit_values.csv")
