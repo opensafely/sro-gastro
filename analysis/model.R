831273bb7281c3b57d45490037e25be5e3cc26d3
Nick Kennedy <nick@nick-kennedy.com>
2021-04-23 16:30:24 +0100

    Add some initial diagnostics to understand FIT

--- analysis/model.R
+++ analysis/model.R
@@ -18,6 +18,7 @@ input %>%
     qfit_result = med_iqr(qfit[!is.na(qfit_date)]),
     had_ft_colorectal_referral = sum(!is.na(ft_referral_date)),
     had_ft_colorectal_clinic = sum(!is.na(ft_clinic_date)),
+    had_ft_colorectal_clinic2 = sum(na.omit(ft_clinic)),
     had_crc_diagnosis = sum(!is.na(crc_diagnosis_date)),
     had_crc_diagnosis2 = sum(na.omit(crc_diagnosis)),
     referral_if_qfit_ge10 = sum(!is.na(ft_referral_date[qfit >= 10 & !is.na(qfit_date)])),
@@ -27,7 +28,12 @@ input %>%
     qfit_clinic_timing = med_iqr((qfit_date %--% ft_clinic_date) / ddays(1), na.rm = TRUE),
     qfit_diagnosis_timing = med_iqr((qfit_date %--% crc_diagnosis_date) / ddays(1), na.rm = TRUE),
     qfit_age = med_iqr(age[!is.na(qfit_date)]),
-    qfit_female = sum(sex[!is.na(qfit_date)] == "F")
+    qfit_female = sum(sex[!is.na(qfit_date)] == "F"),
+    top_four_fit_values = table(round(qfit[!is.na(qfit_date)], 0)) %>%
+      sort(decreasing = TRUE) %>%
+      head(4) %>% 
+      sprintf("%s:%d", names(.), .) %>%
+      paste(collapse = "; ")
   ) %>% 
   write_csv("output/qfit_by_stp.csv")
 
